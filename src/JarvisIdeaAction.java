0861459314d5d1ed00cdc8ef37746c65ae79d1d9
honghuan.Liu <honghuan0921@gmail.com>
2022-09-03 21:36:32 +0800

    First commit

---
+++ src/JarvisIdeaAction.java
@@ -0,0 +1,14 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/9/3 21:28
+ */
+public class JarvisIdeaAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
